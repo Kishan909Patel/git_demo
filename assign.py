010df3215e1d7c03939cc9f1279ac58ca9db0749
Dhruv Patel <dmpkspatel.ce1412@gmail.com>
2021-03-29 18:49:08 +0530

    Implementation way 2.

--- assign.py
+++ assign.py
@@ -40,11 +40,11 @@ for i in range(1,6):
         print(i,end=" ")
       print()
       
-#
+--
 @ @
-# # #
+-- -- --
 @ @ @ @
-# # # # # 
+-- -- -- -- -- 
 @ @ @ @ @ @ 
 for i in range(1,7):
     for j in range(1,i+1):
